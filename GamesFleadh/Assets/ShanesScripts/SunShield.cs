6745fba0a608960ebd0ae65d6b6543066214389f
Shnae <A00208135@U212-A16.STUDENTNET.ait.ie>
2016-02-23 15:26:07 +0000

    Created a shield for the sun object that destroys when all planets are destroyed - SD

---
+++ GamesFleadh/Assets/ShanesScripts/SunShield.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunShield : MonoBehaviour {
+
+	public int health = 1000;
+	public GameObject[] planets = GameObject.FindGameObjectsWithTag("Planet");
+
+	void Update ()
+	{
+		if (planets.Length <= 0) {
+			Destroy (gameObject);
+		}
+
+	}
+
+}
