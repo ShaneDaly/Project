878732aa5079ea9daafe7683c4499bab7d76096e
james gallagher <jamesgllghr@hotmail.com>
2016-02-23 12:13:53 +0000

    no message

--- GamesFleadh/Assets/Jims Assets/Scripts/pointer.cs
+++ GamesFleadh/Assets/Jims Assets/Scripts/pointer.cs
@@ -5,26 +5,34 @@ public class pointer : MonoBehaviour
 {
     private RaycastHit hitInfo;
     public GameObject mCamera;
+    public GameObject sun;
+    public Vector3 border;
+    private float distance;
 
 	void Update () 
     {
         Plane plane = new Plane(Vector3.up, 0);
-
         float dist;
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         if (plane.Raycast(ray, out dist))
         {
             bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
             Vector3 point = ray.GetPoint(dist);
-            if (!hit)
+
+            if ((Vector3.Distance(border, point)) < (Vector3.Distance(border, sun.transform.position)))
             {
-                transform.position = new Vector3(point.x, -50, point.z);
-            }
-            if (hit)
-            {
-                if (hitInfo.transform.gameObject.tag == "Planet")
+                if (!hit)
                 {
-                    
+                    transform.position = new Vector3(point.x, -50, point.z);
+                }
+                if (hit)
+                {
+                    if (hitInfo.transform.gameObject.tag == "Planet")
+                    {
+
+                    }
                 }
             }
         }
@@ -46,5 +54,11 @@ public class pointer : MonoBehaviour
                 }
             }
         }
-	}
+    }
+    private void OnDrawGizmos()
+    {
+        distance = Vector3.Distance(border, sun.transform.position);
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.DrawWireDisc(sun.transform.position, Vector3.up, distance);
+    }
 }
