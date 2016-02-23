ca690d0422ed37a6e4b1f19ea8c854d07531a4f4
Shnae <A00208135@U212-A16.STUDENTNET.ait.ie>
2016-02-23 15:48:26 +0000

    Made a few more edits to scripts - SD

--- GamesFleadh/Assets/ShanesScripts/SunShield.cs
+++ GamesFleadh/Assets/ShanesScripts/SunShield.cs
@@ -6,6 +6,10 @@ public class SunShield : MonoBehaviour {
 	public int health = 1000;
 	public GameObject[] planets = GameObject.FindGameObjectsWithTag("Planet");
 
+	void OnBecameInvisible() {
+		enabled = false;
+	}
+
 	void Update ()
 	{
 		if (planets.Length <= 0) {
