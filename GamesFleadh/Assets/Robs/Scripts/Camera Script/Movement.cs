128039dab6071cbb19a42ed1dc73c085e4edcecb
unknown <A00200429@U212-A10.studentnet.ait.ie>
2016-02-24 14:15:14 +0000

    Added new folder with game assets

---
+++ GamesFleadh/Assets/Robs/Scripts/Camera Script/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	float speed = 50;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+		}
+	}
+}
---
+++ GamesFleadh/Assets/Robs/Scripts/ReverseOrbit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReverseOrbit : MonoBehaviour 
+{
+	public float RotationSpeed = -100f;
+	public float OrbitSpeed = 50f;
+	public float DesiredMoonDistance;
+	public Transform target;
+	
+	void Start () 
+	{
+		DesiredMoonDistance = Vector3.Distance(target.position, transform.position);
+	}
+	
+	void Update () 
+	{
+		transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+		transform.RotateAround(target.position, Vector3.up, OrbitSpeed * Time.deltaTime);
+		
+		//fix possible changes in distance
+		float currentMoonDistance = Vector3.Distance(target.position, transform.position);
+		Vector3 towardsTarget = transform.position - target.position;
+		transform.position += (DesiredMoonDistance - currentMoonDistance) * towardsTarget.normalized;
+	}
+}
