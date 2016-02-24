ccb2bb55014b1d3ea9495c00c2a468a1c8397dc3
Shnae <A00208135@U212-A16.STUDENTNET.ait.ie>
2016-02-24 13:46:42 +0000

    Created a simple game over screen with buttons that do not function yet Fixed the enemy script. Fixed the health script. Fixed the enemy spawner. Created a new shooting script for planets - SD

--- GamesFleadh/Assets/ShanesScripts/DestroyByContact.cs
+++ GamesFleadh/Assets/ShanesScripts/DestroyByContact.cs
@@ -4,17 +4,12 @@ using System.Collections;
 public class DestroyByContact : MonoBehaviour
 {
 	public GameObject other;
-	public int health = 100;
+
 
 	void OnTriggerEnter(Collider other)
 	{
 		Destroy(other);
-		if (health >= 1) {
-			health -= 1;
-			if (health <= 0){
-				Destroy (gameObject);
-			}
-		}
+
 	}
 	
 }
---
+++ GamesFleadh/Assets/ShanesScripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int health = 100;
+	public int damage;
+	public GameObject rocket;
+	public GameObject scrap;
+
+	void Awake ()
+	{
+		rocket = GameObject.FindGameObjectWithTag("Rocket") ;
+	}
+
+	void OnTriggerEnter (Collider rocket){
+		health -= 10;
+	}
+
+	void Update () {
+
+		if (health <= 0) {
+			Destroy (gameObject);
+			GetComponent<Spawner>().max -= 1;
+			Instantiate(scrap, transform.position, transform.rotation);
+		}
+	
+	}
+}
