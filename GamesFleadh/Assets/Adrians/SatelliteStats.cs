8c00735cebfde825ca50435eb3ce113f6e7491b5
unknown <A00209976@U212-A15.studentnet.ait.ie>
2016-02-24 15:42:50 +0000

    Satellites selectable, upgradable and show stats on the HUD [AMC]

--- GamesFleadh/Assets/Adrians/SatelliteStats.cs
+++ GamesFleadh/Assets/Adrians/SatelliteStats.cs
@@ -4,10 +4,8 @@ using System.Collections;
 public class SatelliteStats : MonoBehaviour
 {
 
-    public string satelliteName = null;
     public int offence = 0;
     public int defence = 0;
-    public int utility = 0;
     public int cost = 20;
     public bool active = true;
 
@@ -20,10 +18,5 @@ public class SatelliteStats : MonoBehaviour
     {
         Debug.Log("defence upgraded!");
         defence += 1;
-    }
-    public void upgradeUtility()
-    {
-        Debug.Log("utility upgraded!");
-        utility += 1;
     }
 }
