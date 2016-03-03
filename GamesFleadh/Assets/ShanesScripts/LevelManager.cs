92111357e901f586731ec135890244d610483e3d
Shnae <A00208135@student.ait.ie>
2016-03-03 09:18:10 +0000

    More minor edits. Still working on making new wave of enemies stronger - SD

--- GamesFleadh/Assets/ShanesScripts/LevelManager.cs
+++ GamesFleadh/Assets/ShanesScripts/LevelManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class LevelManager : MonoBehaviour
 {
 	private static string lastLevel;
-	public static string nextLevel;
 
 	public static void setLastLevel(string level)
 	{
