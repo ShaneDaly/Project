a6a4554985a645b50ac821d09a81ddc30adb64d8
Shnae <A00208135@student.ait.ie>
2016-03-04 11:53:01 +0000

    Fixed the health bar and began work on a script that allows it to be set visible or not - SD

---
+++ GamesFleadh/Assets/OnOff.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnOff : MonoBehaviour {
+
+	public enum State {On, Off};
+	public State state;
+
+	void On ()
+	{
+		gameObject.SetActive (true);
+	}
+	void Off ()
+	{
+		gameObject.SetActive (false);
+	}
+
+	void Update () {
+
+
+		switch (state) 
+		{
+		case(State.On):
+		{
+			On ();
+			break;
+		}
+		case(State.Off):
+		{
+			Off ();
+			break;
+		}
+
+		}
+		if (Input.GetKeyDown("space"))
+		{
+			if(state == State.On)
+			{
+				state = State.Off;
+			}
+			if(state == State.Off)
+			{
+				state = State.On;
+			}
+		}
+
+	}
+}
